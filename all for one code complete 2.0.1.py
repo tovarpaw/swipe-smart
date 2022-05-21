ef5b4635196163f9ba3d572d202e25f498b08c0e
TrevorCollard <105944481+TrevorCollard@users.noreply.github.com>
2022-05-21 11:57:04 -0500

    SwipeSmart_V2.0

--- all for one code complete 2.0.1.py
+++ all for one code complete 2.0.1.py
@@ -4,89 +4,50 @@ import utime
 potentiometer = machine.ADC(26)
 servo = machine.PWM(machine.Pin(15))
 servo.freq(50)
-value= potentiometer.read_u16()
+value = potentiometer.read_u16()
 button = machine.Pin(13, machine.Pin.IN)
 
+# this function controls the interval and how far the wiper will go
 def interval_mapping(x, in_min, in_max, out_min, out_max):
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
-def servo_write(pin,angle):
-    pulse_width=interval_mapping(angle, 0, 180, 0.5,2.5)
-    duty=int(interval_mapping(pulse_width, 0, 20, 0,65535))
+# this function uses the interval function to control the servo
+def servo_write(pin, angle):
+    pulse_width = interval_mapping(angle, 0, 180, 0.5,2.5)
+    duty = int(interval_mapping(pulse_width, 0, 20, 0,65535))
     pin.duty_u16(duty)
 
+# master loop that runs forever, it calls servo_write and uses if statements to measure voltage using the potentiometer
 while True:
+    # we are working on including a button which turns the wipers on and off
     # while button == 1:
         # button = machine.Pin(13, machine.Pin.IN)
         # print("You pressed the button!")
         value= potentiometer.read_u16()
         if value > 64000:
             for angle in range(120):
-                servo_write(servo,angle)
+                servo_write(servo, angle)
                 utime.sleep_ms(10)
             for angle in range(120,-1,-1):
-                servo_write(servo,angle)
+                servo_write(servo, angle)
                 utime.sleep_ms(10)
             utime.sleep_ms(5000)
-            print("slow",value)
+            print("slow speed:", value)
         elif value < 11000:
             for angle in range(120):
-                servo_write(servo,angle)
+                servo_write(servo, angle)
                 utime.sleep_ms(10)
             for angle in range(120,-1,-1):
-                servo_write(servo,angle)
+                servo_write(servo, angle)
                 utime.sleep_ms(10)
             utime.sleep_ms(1000)
-            print("high",value)
+            print("high speed:", value)
         else:
             for angle in range(120):
-                servo_write(servo,angle)
+                servo_write(servo, angle)
                 utime.sleep_ms(10)
             for angle in range(120,-1,-1):
-                servo_write(servo,angle)
+                servo_write(servo, angle)
                 utime.sleep_ms(10)
             utime.sleep_ms(3000)
-            print("mid",value)
-        # utime.sleep(1)
-
-
-
-# Message #resources
-
-
-
-# potentiometer = machine.ADC(26)
-# servo = machine.PWM(machine.Pin(15))
-# servo.freq(50)
-
-# def interval_mapping(x, in_min, in_max, out_min, out_max):
-    # return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
-
-# def servo_write(pin,angle):
-    # pulse_width=interval_mapping(angle, 0, 180, 0.5,2.5)
-    # duty=int(interval_mapping(pulse_width, 0, 20, 0,65535))
-    # pin.duty_u16(duty)
-
-# while True:
-    # value=potentiometer.read_u16()
-    # angle=interval_mapping(value,0,65535,0,180)
-    # servo_write(servo,angle)
-    # print("value ",value)
-    
-    # # if value > 60000:
-        # # servo.duty_u16(0)
-        # # utime.sleep_ms(5000)
-        # # servo.duty_u16(120)
-        # # utime.sleep_ms(500)
-    # # if  value > 20000:
-        # # servo.duty_u16(0)
-        # # utime.sleep_ms(3000)
-        # # servo.duty_u16(120)
-        # # utime.sleep_ms(500)    
-    # # if value < 20000:
-        # # servo.duty_u16(0)
-        # # utime.sleep_ms(1000)
-        # # servo.duty_u16(120)
-        # # utime.sleep_ms(500)
-
-    # utime.sleep_ms(200)
+            print("medium speed:", value)
